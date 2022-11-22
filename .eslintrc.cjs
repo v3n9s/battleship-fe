79ad3990a58fde4d4feb23ce12afd2c419e0cc8e
Venyamin Shvets <venyashvets@gmail.com>
2022-11-22 11:29:11 +0300

    build: configure eslint and prettier

---
+++ .eslintrc.cjs
@@ -0,0 +1,44 @@
+/**
+ * @type {import('eslint').Linter.Config}
+ */
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+    browser: true,
+  },
+  ignorePatterns: ['.eslintrc.cjs', 'webpack.config.cjs'],
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:@typescript-eslint/recommended-requiring-type-checking',
+        'plugin:react/recommended',
+        'plugin:react/jsx-runtime',
+        'plugin:react-hooks/recommended',
+        'plugin:prettier/recommended',
+      ],
+      parserOptions: {
+        project: './tsconfig.json',
+        tsconfigRootDir: __dirname,
+      },
+      settings: {
+        react: {
+          version: 'detect',
+        },
+      },
+      rules: {
+        '@typescript-eslint/no-misused-promises': [
+          'error',
+          {
+            checksConditionals: true,
+            checksVoidReturn: false,
+            checksSpreads: true,
+          },
+        ],
+      },
+    },
+  ],
+};
