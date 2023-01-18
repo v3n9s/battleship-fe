a96f35232737c7112d91d4d0cb25fc13c295537c
Venyamin Shvets <venyashvets@gmail.com>
2023-01-18 17:00:45 +0300

    feat: add styles to Rooms component

--- src/components/Rooms.tsx
+++ src/components/Rooms.tsx
@@ -5,6 +5,23 @@ import RoomsItem from './RoomsItem';
 import { UserDataContext } from '../contexts/user-data';
 import { RoomsContext } from '../contexts/rooms';
 import { useWs } from '../hooks/ws';
+import styled from 'styled-components';
+
+const StyledRooms = styled.div`
+  padding: 10px;
+`;
+
+const RoomsList = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 25px;
+  margin-top: 25px;
+`;
+
+const StatusText = styled.div`
+  text-align: center;
+  font-size: 24px;
+`;
 
 const Rooms: FC = () => {
   const { state, dispatch } = useContext(RoomsContext);
@@ -21,7 +38,7 @@ const Rooms: FC = () => {
   );
 
   return (
-    <>
+    <StyledRooms>
       <Form
         fields={{
           name: {
@@ -38,12 +55,16 @@ const Rooms: FC = () => {
         onSubmit={createRoom}
         submitButtonText="Create room"
       />
-      {!state.serverResponded
-        ? 'Loading...'
-        : state.rooms.length === 0
-        ? 'There is no rooms yet'
-        : state.rooms.map((room) => <RoomsItem key={room.id} room={room} />)}
-    </>
+      <RoomsList>
+        {!state.serverResponded ? (
+          <StatusText>Loading...</StatusText>
+        ) : state.rooms.length === 0 ? (
+          <StatusText>There is no rooms yet</StatusText>
+        ) : (
+          state.rooms.map((room) => <RoomsItem key={room.id} room={room} />)
+        )}
+      </RoomsList>
+    </StyledRooms>
   );
 };
 
