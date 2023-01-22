eb58d5628004dee18ec93c20a20a74727173653f
Venyamin Shvets <venyashvets@gmail.com>
2023-01-22 21:11:16 +0300

    feat: implement borderless prop in Button component

--- src/components/styled/Button.tsx
+++ src/components/styled/Button.tsx
@@ -3,22 +3,30 @@ import styled, { css } from 'styled-components';
 const Button = styled.button.attrs<
   {
     isLoading?: boolean | undefined;
+    borderless?: boolean | undefined;
   },
   {
     disabled: boolean | undefined;
     isLoading: boolean;
+    borderless: boolean;
   }
 >((props) => ({
   disabled: props.disabled ?? props.isLoading,
   isLoading: props.isLoading ?? false,
+  borderless: props.borderless ?? false,
 }))`
   display: block;
   padding: 5px;
   font-size: 20px;
   color: ${(props) => props.theme.secondaryColor};
-  border: 2px solid
-    ${(props) =>
-      props.isLoading ? props.theme.loadingColor : props.theme.primaryColor};
+  ${(props) =>
+    props.borderless
+      ? ''
+      : css`
+          border: 2px solid
+            ${(p) =>
+              props.isLoading ? p.theme.loadingColor : p.theme.primaryColor};
+        `}
   background-color: ${(props) =>
     props.isLoading ? props.theme.loadingColor : props.theme.primaryColor};
   transition: background-color 0.15s linear, border 0.15s linear;
