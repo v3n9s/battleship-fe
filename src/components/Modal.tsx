0259ee8347e68105d17fc81f863a673b4ff802e5
Venyamin Shvets <venyashvets@gmail.com>
2023-01-23 21:17:09 +0300

    feat: edit Modal component styles

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -20,21 +20,32 @@ const StyledModal = styled.div`
   position: relative;
   padding: 0px 10px;
   pointer-events: none;
-
-  > * {
-    pointer-events: auto;
-  }
 `;
 
-const Modal: FC<{ children: ReactNode; closeModal: () => void }> = ({
-  children,
-  closeModal,
-}) => {
+const StyledModalBody = styled.div<{ maxWidth?: number | undefined }>`
+  margin: 10px auto;
+  max-width: ${(props) =>
+    props.maxWidth ? props.maxWidth : props.theme.mainContainerMaxWidth}px;
+  max-height: calc(100vh - 20px);
+  padding: 10px;
+  background-color: ${(props) => props.theme.backgroundColor};
+  border: 2px solid ${(props) => props.theme.primaryColor};
+  pointer-events: auto;
+  overflow-y: auto;
+`;
+
+const Modal: FC<{
+  children: ReactNode;
+  closeModal: () => void;
+  maxWidth?: number | undefined;
+}> = ({ children, closeModal, maxWidth }) => {
   return (
     <>
       <StyledWrapper>
         <StyledOverlay onClick={closeModal} />
-        <StyledModal>{children}</StyledModal>
+        <StyledModal>
+          <StyledModalBody maxWidth={maxWidth}>{children}</StyledModalBody>
+        </StyledModal>
       </StyledWrapper>
     </>
   );
--- src/components/PasswordModal.tsx
+++ src/components/PasswordModal.tsx
@@ -1,18 +1,8 @@
 import { FC, useCallback, useRef } from 'react';
-import styled from 'styled-components';
 import { useStore } from '../hooks/store';
 import { useWs } from '../hooks/ws';
 import Form from './Form';
 import Modal from './Modal';
-
-const StyledPasswordModal = styled.div`
-  margin: 10px auto;
-  max-width: 400px;
-  width: 100%;
-  padding: 10px;
-  background-color: ${(props) => props.theme.backgroundColor};
-  border: 2px solid ${(props) => props.theme.primaryColor};
-`;
 
 const PasswordModal: FC = () => {
   const { dispatch, state } = useStore();
@@ -44,14 +34,12 @@ const PasswordModal: FC = () => {
   }).current;
 
   return state.passwordModal.roomId ? (
-    <Modal closeModal={closeModal}>
-      <StyledPasswordModal>
-        <Form
-          fields={formFields}
-          onSubmit={join(state.passwordModal.roomId)}
-          submitButtonText="join"
-        />
-      </StyledPasswordModal>
+    <Modal closeModal={closeModal} maxWidth={400}>
+      <Form
+        fields={formFields}
+        onSubmit={join(state.passwordModal.roomId)}
+        submitButtonText="join"
+      />
     </Modal>
   ) : null;
 };
