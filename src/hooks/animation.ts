56e79adaa46977f64ca463e1c06663e9f3f0c578
Venyamin Shvets <venyashvets@gmail.com>
2023-01-30 15:57:26 +0300

    feat: implement useAnimation hook

---
+++ src/hooks/animation.ts
@@ -0,0 +1,45 @@
+import { RefObject, useCallback, useEffect, useRef, useState } from 'react';
+
+export type AnimationState = 'entering' | 'entered' | 'exiting';
+
+export const useAnimation = (
+  elemRef: RefObject<HTMLElement>,
+  onExit?: () => void,
+) => {
+  const [state, setState] = useState<AnimationState>('entering');
+
+  const ongoingAnimationsCount = useRef(0);
+
+  useEffect(() => {
+    if (state === 'entering' || state === 'exiting') {
+      const elem = elemRef.current;
+      if (elem) {
+        const onAnimationStart = () => {
+          ongoingAnimationsCount.current++;
+        };
+        const onAnimationEnd = () => {
+          ongoingAnimationsCount.current--;
+          if (ongoingAnimationsCount.current === 0) {
+            if (state === 'entering') {
+              setState('entered');
+            } else {
+              onExit?.();
+            }
+          }
+        };
+        elem.addEventListener('animationstart', onAnimationStart);
+        elem.addEventListener('animationend', onAnimationEnd);
+        return () => {
+          elem.removeEventListener('animationstart', onAnimationStart);
+          elem.removeEventListener('animationend', onAnimationEnd);
+        };
+      }
+    }
+  }, [state, elemRef, onExit, ongoingAnimationsCount]);
+
+  const exit = useCallback(() => {
+    setState('exiting');
+  }, []);
+
+  return { state, exit };
+};
