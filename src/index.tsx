a4ef1f4f5266093d4e8b2b627c048dbc8cebcf12
Venyamin Shvets <venyashvets@gmail.com>
2023-02-12 00:15:07 +0300

    feat: wrap App with StrictMode

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,9 @@
+import { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/App';
 
-ReactDOM.createRoot(document.querySelector('#root') as Element).render(<App />);
+ReactDOM.createRoot(document.querySelector('#root') as Element).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+);
