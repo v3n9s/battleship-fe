ac4900174792788985f84d8533e1151e1580439e
Venyamin Shvets <venyashvets@gmail.com>
2023-02-12 14:06:37 +0300

    refactor: delete unused file

--- src/services/ws-connection.ts
+++
@@ -1,7 +0,0 @@
-import config from '../config';
-
-let ws: WebSocket;
-
-export const getWsConnection = (token: string) => {
-  return ws || (ws = new WebSocket(`${config.wsBackendUrl}/?token=${token}`));
-};
