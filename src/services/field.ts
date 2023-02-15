88585f19ae0316a5233f4f93203a27934c3440c6
Venyamin Shvets <venyashvets@gmail.com>
2023-02-15 15:08:25 +0300

    refactor: move field manipulation to separate file

---
+++ src/services/field.ts
@@ -0,0 +1,96 @@
+import { Field } from '../types';
+
+export const getEmptyField = () => {
+  return new Array(10)
+    .fill(new Array(10).fill(false))
+    .map((v) => [...(v as boolean[])]);
+};
+
+const getCellsAroundCell = ([colInd, rowInd]: [number, number]): [
+  number,
+  number,
+][] => {
+  return [
+    [colInd - 1, rowInd - 1],
+    [colInd - 1, rowInd],
+    [colInd - 1, rowInd + 1],
+    [colInd, rowInd - 1],
+    [colInd, rowInd + 1],
+    [colInd + 1, rowInd - 1],
+    [colInd + 1, rowInd],
+    [colInd + 1, rowInd + 1],
+  ];
+};
+
+const getCellsAroundCells = (cells: [number, number][]): [number, number][] => {
+  return cells
+    .map((cell) => getCellsAroundCell(cell))
+    .flat()
+    .reduce((acc, [rowInd, colInd]) => {
+      if (
+        !acc.some(
+          ([uniqueRowInd, uniqueColInd]) =>
+            uniqueRowInd === rowInd && uniqueColInd === colInd,
+        ) &&
+        !cells.some(
+          ([shipRowInd, shipColInd]) =>
+            shipRowInd === rowInd && shipColInd === colInd,
+        )
+      ) {
+        acc.push([rowInd, colInd]);
+      }
+      return acc;
+    }, [] as [number, number][]);
+};
+
+const isFree = (field: Field, cells: [number, number][]) => {
+  return [...cells, ...getCellsAroundCells(cells)].every(
+    ([rowInd, colInd]) => !field[rowInd]?.[colInd],
+  );
+};
+
+const getRandomShip = (size: number) => {
+  const isHorizontal = Math.random() > 0.5;
+  let rowInd = Math.floor(Math.random() * 10);
+  let colInd = Math.floor(Math.random() * (10 - size + 1));
+  if (isHorizontal) {
+    [rowInd, colInd] = [colInd, rowInd];
+  }
+  return new Array(size)
+    .fill([rowInd, colInd])
+    .map(
+      ([rowInd, colInd]: [number, number], i) =>
+        (isHorizontal ? [rowInd + i, colInd] : [rowInd, colInd + i]) as [
+          number,
+          number,
+        ],
+    );
+};
+
+const setRandomShips = (field: Field, size: number, amount: number) => {
+  if (amount === 0) {
+    return;
+  }
+
+  let shipCells;
+  do {
+    shipCells = getRandomShip(size);
+  } while (!isFree(field, shipCells));
+
+  shipCells.forEach(([rowInd, y]) => {
+    const row = field[rowInd];
+    if (row) {
+      row[y] = true;
+    }
+  });
+  setRandomShips(field, size, amount - 1);
+};
+
+export const getRandomField = () => {
+  const field = getEmptyField();
+  setRandomShips(field, 4, 1);
+  setRandomShips(field, 3, 2);
+  setRandomShips(field, 2, 3);
+  setRandomShips(field, 1, 4);
+  return field;
+};
